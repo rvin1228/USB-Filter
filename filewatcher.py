46c406a9deaf0f94d3142ad65a24a6248937967f
Arvin Rafael Legaspi <64195231+rvin1228@users.noreply.github.com>
2023-04-30 21:57:00 +0800

    Update filewatcher.py
    Changed src_path, wherein it can now monitor all connected USB drives to the RPi. Also added saving of received data through data.txt and info.txt.

--- filewatcher.py
+++ filewatcher.py
@@ -1,4 +1,4 @@
-import os, shutil, hashlib, inotify.adapters
+import os, shutil, hashlib, inotify.adapters, datetime, re, psutil
  
 #getting the MD5 hash value of a file
 def get_md5_hash(file_path):
@@ -15,9 +15,14 @@ def monitor_file_transfer(src_path, dest_path, qua_path):
  
     i = inotify.adapters.Inotify() #use i to initialize monitoring of file system
  
-    i.add_watch(src_path) #monitor source path
-    i.add_watch(dest_path) #monitor destination path
+    drives = os.listdir('/media/raspberry')
+    for drive in drives:
+        src_path = os.path.join('/media/raspberry', drive)
+        if os.path.isdir(src_path):
+            i.add_watch(src_path)
+        print(drive)
  
+    i.add_watch(dest_path) #monitor destination path
  
     #provide information for file system events
     for event in i.event_gen(yield_nones=False):
@@ -37,21 +42,47 @@ def monitor_file_transfer(src_path, dest_path, qua_path):
                 existHash = "84c82835a5d21bbcf75a61706d8ab549" #MD5 hash value of WannaCry
                 md5_hash = get_md5_hash(file_path) #get MD5 hash value of file
  
+                with open("info.txt", "r") as file: #for historical data information
+                    file_contents = file.read()
+                malwareCount = int(re.search(r"Malware Count: (\d+)", file_contents).group(1))
+                safeCount = int(re.search(r"Safe Count: (\d+)", file_contents).group(1))
+ 
+                data = open("data.txt", "a") #append details to output file
+                data.write("\nFilename: " + str(filename))
+                data.write("\nFile Path: " + file_path)
+                file_size = os.path.getsize(file_path)
+                data.write("\nFile Size: " + str(file_size) + " bytes")
+                data.write("\nMD5 Hash Value: " + md5_hash)
+                now = datetime.datetime.now()
+                data.write(f"\nDate Detected: {now.strftime('%Y-%m-%d %H:%M:%S')}")
+ 
                 #check MZPE file structure and compare MD5 hash value
                 if PE > index > MZ or existHash == md5_hash:
+                    status = "Quarantine"
                     print(f"Blocking transfer of {filename}")
  
                     qua_filename = filename.replace('.exe','.exe.lock') #rename file
                     qua_file_path = os.path.join(qua_path, qua_filename)
                     shutil.move(file_path, qua_file_path) #quarantine file to quarantine folder
                     print(f"MD5 hash of {filename}: {md5_hash}")
+                    malwareCount += 1
+ 
                 else:
+                    status = "Safe"
                     print(f"File transfer allowed: {filename}")
                     print(f"MD5 hash of {filename}: {md5_hash}")
+                    safeCount += 1
+ 
+                data.write("\nStatus: " + status + "\n")
+                data.close()
+                file_contents = re.sub(r"Malware Count: \d+", f"Malware Count: {malwareCount}", file_contents)
+                file_contents = re.sub(r"Safe Count: \d+", f"Safe Count: {safeCount}", file_contents)
+                with open("info.txt", "w") as file:
+                    file.write(file_contents)
  
 if __name__ == "__main__":
-    src_path = "/media/raspberry/VIN"
-    dest_path = "/home/raspberry/Desktop/folder"
+    src_path = None
+    dest_path = "/home/raspberry/Desktop"
     qua_path = "/home/raspberry/Desktop/Quarantine"
  
     monitor_file_transfer(src_path, dest_path, qua_path)
